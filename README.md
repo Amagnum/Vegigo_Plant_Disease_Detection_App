570f951ad8e799459a242d1269f1f2f7d7aadf82
Amagnum <cse180001001@iiti.ac.in>
2020-01-01 18:17:21 +0530

    update

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # vegigo_plant_disease_detection_app
-We created a system that can help maintain home plants or even a full farm — giving our farmers the power of automated AI system to sustain their farm health. Indian agriculture farmers generally suffer due to the low production of crops. The problems lie in conveying a proper message and guidance to maintain their fields efficiently.
 
-Here we can see our model in action on a mobile phone, Right now the application provides the probability of a disease, we can easily provide the best way to fight against the predicted disease.
+A new Flutter project.
 
-The image can be provided to the model via both a camera and an existing file on the phone.
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,391 @@
+// import 'dart:io';
+
+// import 'package:flutter/material.dart';
+// import 'package:tflite/tflite.dart';
+// import 'package:camera/camera.dart';
+// import 'package:image_picker/image_picker.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         // This is the theme of your application.
+//         //
+//         // Try running your application with "flutter run". You'll see the
+//         // application has a blue toolbar. Then, without quitting the app, try
+//         // changing the primarySwatch below to Colors.green and then invoke
+//         // "hot reload" (press "r" in the console where you ran "flutter run",
+//         // or simply save your changes to "hot reload" in a Flutter IDE).
+//         // Notice that the counter didn't reset back to zero; the application
+//         // is not restarted.
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(title: 'Plant Health Detector'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
+
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
+
+//   final String title;
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   File _image;
+
+//   // String res = await Tflite.loadModel(
+//   // model: "assets/mobilenet_v1_1.0_224.tflite",
+//   // labels: "assets/labels.txt",
+//   // numThreads: 1 // defaults to 1
+//   // );
+
+//   Future getImage() async {
+//     var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+//     setState(() {
+//       _image = image;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Container(
+//               height: 300,
+//               child: _image == null
+//               ? Text('No image selected.')
+//               : Image.file(_image),
+//             ),
+//             RaisedButton(
+//               onPressed: (){
+//                 getImage();
+//               },
+//               child: Container(
+//                 child: Text("Open Camera"),
+//               ),
+//             ),
+//             RaisedButton(
+//               onPressed: (){
+
+//               },
+//               child: Container(
+//                 child: Text("Select image from Library"),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'dart:async';
+import 'dart:io';
+import 'dart:math';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
+
+import 'package:tflite/tflite.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() => runApp(new App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  File _image;
+  List _recognitions;
+  double _imageHeight;
+  String _model;
+  double _imageWidth;
+  bool _busy = false;
+
+  Future predictImagePicker() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (image == null) return;
+    setState(() {
+      _busy = true;
+    });
+    predictImage(image);
+  }
+
+  Future predictImage(File image) async {
+    if (image == null) return;
+
+    await model(image);
+
+    new FileImage(image)
+        .resolve(new ImageConfiguration())
+        .addListener(ImageStreamListener((ImageInfo info, bool _) {
+      setState(() {
+        _imageHeight = info.image.height.toDouble();
+        _imageWidth = info.image.width.toDouble();
+      });
+    }));
+
+    setState(() {
+      _image = image;
+      _busy = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _busy = true;
+
+    loadModel().then((val) {
+      setState(() {
+        _busy = false;
+      });
+    });
+  }
+
+  Future loadModel() async {
+    Tflite.close();
+    try {
+      String res;
+      res = await Tflite.loadModel(
+        model: "assets/converted_model.tflite",
+        labels: "assets/labels.txt",
+      );
+      print(res);
+    } on PlatformException {
+      print('Failed to load model.');
+    }
+  }
+
+  Uint8List imageToByteListFloat32(
+      img.Image image, int inputSize, double mean, double std) {
+    var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
+    var buffer = Float32List.view(convertedBytes.buffer);
+    int pixelIndex = 0;
+    for (var i = 0; i < inputSize; i++) {
+      for (var j = 0; j < inputSize; j++) {
+        var pixel = image.getPixel(j, i);
+        buffer[pixelIndex++] = (img.getRed(pixel) - mean) / std;
+        buffer[pixelIndex++] = (img.getGreen(pixel) - mean) / std;
+        buffer[pixelIndex++] = (img.getBlue(pixel) - mean) / std;
+      }
+    }
+    return convertedBytes.buffer.asUint8List();
+  }
+
+  Uint8List imageToByteListUint8(img.Image image, int inputSize) {
+    var convertedBytes = Uint8List(1 * inputSize * inputSize * 3);
+    var buffer = Uint8List.view(convertedBytes.buffer);
+    int pixelIndex = 0;
+    for (var i = 0; i < inputSize; i++) {
+      for (var j = 0; j < inputSize; j++) {
+        var pixel = image.getPixel(j, i);
+        buffer[pixelIndex++] = img.getRed(pixel);
+        buffer[pixelIndex++] = img.getGreen(pixel);
+        buffer[pixelIndex++] = img.getBlue(pixel);
+      }
+    }
+    return convertedBytes.buffer.asUint8List();
+  }
+
+  Future recognizeImage(File image) async {
+    var recognitions = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 6,
+      threshold: 0.05,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _recognitions = recognitions;
+    });
+  }
+
+  Future model(File image) async {
+    var recognitions = await Tflite.runModelOnImage(
+      path: image.path,
+      threshold: 0.3,
+      imageMean: 0.0,
+      imageStd: 255.0,
+      numResults: 1,
+    );
+    setState(() {
+      _recognitions = recognitions;
+    });
+  }
+
+  onSelect(model) async {
+    setState(() {
+      _busy = true;
+      _model = model;
+      _recognitions = null;
+    });
+    await loadModel();
+
+    if (_image != null)
+      predictImage(_image);
+    else
+      setState(() {
+        _busy = false;
+      });
+  }
+
+  List<Widget> renderBoxes(Size screen) {
+    if (_recognitions == null) return [];
+    if (_imageHeight == null || _imageWidth == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageWidth * screen.width;
+    Color blue = Color.fromRGBO(37, 213, 253, 1.0);
+    return _recognitions.map((re) {
+      return Positioned(
+        left: re["rect"]["x"] * factorX,
+        top: re["rect"]["y"] * factorY,
+        width: re["rect"]["w"] * factorX,
+        height: re["rect"]["h"] * factorY,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+            border: Border.all(
+              color: blue,
+              width: 2,
+            ),
+          ),
+          child: Text(
+            "${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+            style: TextStyle(
+              background: Paint()..color = blue,
+              color: Colors.white,
+              fontSize: 12.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  List<Widget> renderKeypoints(Size screen) {
+    if (_recognitions == null) return [];
+    if (_imageHeight == null || _imageWidth == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageWidth * screen.width;
+
+    var lists = <Widget>[];
+    _recognitions.forEach((re) {
+      var color = Color((Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+          .withOpacity(1.0);
+      var list = re["keypoints"].values.map<Widget>((k) {
+        return Positioned(
+          left: k["x"] * factorX - 6,
+          top: k["y"] * factorY - 6,
+          width: 100,
+          height: 12,
+          child: Container(
+            child: Text(
+              "● ${k["part"]}",
+              style: TextStyle(
+                color: color,
+                fontSize: 12.0,
+              ),
+            ),
+          ),
+        );
+      }).toList();
+
+      lists..addAll(list);
+    });
+
+    return lists;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    List<Widget> stackChildren = [];
+
+    stackChildren.add(Positioned(
+      top: 0.0,
+      left: 0.0,
+      width: size.width,
+      child: _image == null ? Text('No image selected.') : Image.file(_image),
+    ));
+
+    stackChildren.add(Center(
+      child: Column(
+        children: _recognitions != null
+            ? _recognitions.map((res) {
+                return Text(
+                  "${res["index"]} - ${res["label"]}: ${res["confidence"].toStringAsFixed(3)}",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    background: Paint()..color = Colors.white,
+                  ),
+                );
+              }).toList()
+            : [],
+      ),
+    ));
+
+    if (_busy) {
+      stackChildren.add(const Opacity(
+        child: ModalBarrier(dismissible: false, color: Colors.grey),
+        opacity: 0.3,
+      ));
+      stackChildren.add(const Center(child: CircularProgressIndicator()));
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plant Disease Detector'),
+      ),
+      body: Stack(
+        children: stackChildren,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: predictImagePicker,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.image),
+      ),
+    );
+  }
+}
